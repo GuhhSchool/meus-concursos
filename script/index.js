7dda6ca18d0a83c3b38f53aed430c8f8ba99d65b
Gustavo <107372982+GuhhSchool@users.noreply.github.com>
2024-01-29 16:11:06 -0300

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,4 @@
+if (new Date().getMinutes() % 2 === 0) 
+    document.body.style.backgroundImage = 'url("./assets/background1\ -\ Nandini\ Cherrytree.jpg")';
+else 
+    document.body.style.backgroundImage = 'url("./assets/background\ -\ WallpaperMaiden.jpg")';
